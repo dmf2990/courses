f11a1531e417a3a5d9cf5a52f319cfe26ed5d667
Drew Franklin <dmf2990@gmail.com>
2019-04-27 10:54:54 -0400

    initial

---
+++ src/main/java/io/dmf/courses/CoursesApplication.java
@@ -0,0 +1,13 @@
+package io.dmf.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesApplication.class, args);
+	System.out.println("Drew it works");
+	}
+}
